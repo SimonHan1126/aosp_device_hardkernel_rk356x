e124ba2810a1a04fa5b632398b8a6d9f3c1163a8
Chris <codewalker@hardkernel.com>
2024-07-23 09:16:19 +0900

    ODROID: FIX bdroid_buildconfig.h
    CSR8510 device needs BLE_VND_INCLUDED.
    
    Change-Id: Iffc25a722b27e0889b5f23554a3a8819b9591336

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,11 +19,13 @@
 
 #define BTM_DEF_LOCAL_NAME	"ODROID"
 #define BTA_DM_COD {0x1A, 0x01, 0x10}
-#define DISABLE_WBS TRUE
-#define ESCO_DATA_PATH_PCM 6
+#define BLE_PRIVACY_SPT FALSE
+//#define DISABLE_WBS TRUE
+//#define ESCO_DATA_PATH_PCM 6
 
 /* Odroid custom option */
 #define SUPERVISION_TIMEOUT FALSE
 #define BLE_ADV_FILTER FALSE
 
+#define BLE_VND_INCLUDED FALSE
 #endif
